7b83718b51317cf3a70e246c93c4f1751432a460
SpiveeWorks <spiveehere@gmail.com>
2017-06-28 09:48:40 +1000

    Join.hs implementation
    not tested since the old functions don't even work yet
    (will probably rebase once they work again)

--- Join.hs
+++ Join.hs
@@ -1,4 +1,39 @@
 module Join (main) where
 
+import Control.Monad
+
+import System.Environment
+import System.Directory
+
+import PathCompression
+import LinkParsing
+import LinkIO
+
 main :: IO ()
-main = return ()
+main = do
+  (comp, eval) <- getPathMaps
+  roots <- getArgs
+  forM_ roots $ joinEach comp eval
+
+joinEach :: CompMap -> EvalMap -> FilePath -> IO ()
+joinEach comp eval root = do
+  children <- listDirectory root
+  linkss <- forM children $ getLinksOrRecurse eval
+  let links = concat linkss
+  appendLinksIn comp links root
+
+getLinksOrRecurse :: EvalMap -> FilePath -> IO [Link]
+getLinksOrRecurse eval path = do
+  isFile <- doesFileExist path
+  if isFile
+    then return []
+    else do
+      mDotLink <- getDotLinkIn path
+      case mDotLink of
+        Just dotLink -> getLinks eval dotLink
+        Nothing -> do
+          children <- listDirectory path
+          resultss <- forM children $ getLinksOrRecurse eval
+          return $ concat resultss
+  
+  
