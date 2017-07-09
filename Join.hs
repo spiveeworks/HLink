50ffef3c43ceed5c77869b24076940014a638784
SpiveeWorks <spiveehere@gmail.com>
2017-07-09 10:17:40 +1000

    break up Join implementation

--- Join.hs
+++ Join.hs
@@ -1,4 +1,4 @@
-module Join (main) where
+module Join(main) where
 
 import Control.Monad
 
@@ -6,7 +6,7 @@ import System.Directory
 import System.FilePath
 
 import PathCompression
-import LinkParsing(Link)
+import LinkParsing( Link((:=>:)) )
 import LinkIO
 
 main :: [String] -> IO ()
@@ -16,33 +16,39 @@ main roots = do
 
 joinEach :: CompMap -> EvalMap -> FilePath -> IO ()
 joinEach comp eval root = do
-  links <- catCallContents root $ getLinksOrRecurse eval root
+  dotLinksPaths <- catCallContents root getDotLinksPaths
+  linkss <- forM dotLinksPaths $ adjustConsumeLinks eval root
+  let links = concat linkss
   appendLinksIn comp links root
 
-getLinksOrRecurse :: EvalMap -> FilePath -> FilePath -> IO [Link]
-getLinksOrRecurse eval root path = do
-  isFile <- doesFileExist path       -- there is nothing we can do with a file
-  isLink <- pathIsSymbolicLink path  -- on one hand we don't want loops, 
-                                     -- on the other the target could be intended as separate
+
+
+getDotLinksPaths :: FilePath -> IO [FilePath]
+getDotLinksPaths root = do
+  isFile <- doesFileExist root      -- there is nothing we can do with a file
+  isLink <- pathIsSymbolicLink root -- on one hand we don't want loops, 
+                                    -- on the other the target could be intended as separate
   if isFile || isLink
     then return []
     else do
-      mDotLink <- getDotLinkIn path
-      case mDotLink of
-        Just dotLink -> do
-          links <- getLinksFrom eval dotLink
-          length links `seq` removeFile dotLink
-          return $ map adjust links
-        Nothing -> catCallContents path $ getLinksOrRecurse eval root
+      mDotLinks <- getDotLinkIn root
+      case mDotLinks of
+        Just dotLinks -> return [dotLinks]
+        Nothing -> catCallContents root getDotLinksPaths
+
+adjustConsumeLinks :: EvalMap -> FilePath -> FilePath -> IO [Link]
+adjustConsumeLinks eval root dotLinks = do
+  links <- getLinksFrom eval dotLinks
+  length links `seq` removeFile dotLinks
+  return $ map adjust links
   where adjust link@(source :=>: dest)
           | isRelative source = (normalise $ path' </> source) :=>: dest
           | otherwise = link
-        path' = makeRelative root path
-
+        path' = makeRelative root $ takeDirectory dotLinks
 
 catCallContents :: FilePath -> (FilePath -> IO [a]) -> IO [a]
 catCallContents path action = do
   contents <- listDirectory path
-  let absContents = map (\ name -> normalise $ path </> name) contents
-  linkss <- forM absContents action  -- in theory these don't have to be links
-  return $ concat linkss
+  let absContents = map (normalise . (path </>)) contents
+  resultss <- mapM action absContents
+  return $ concat resultss
