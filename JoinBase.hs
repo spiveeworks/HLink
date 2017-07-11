828f5b404209c2cc4f8ff310f439c89967bc2f45
SpiveeWorks <spiveehere@gmail.com>
2017-07-12 08:59:41 +1000

    compiling

--- JoinBase.hs
+++ JoinBase.hs
@@ -19,9 +19,9 @@ adjustConsumeLinksInFile eval newRoot dotLinks = adjustConsumeLinks eval newRoot
 
 -- When dealing with a symlink, use the target for interpreting impossible links
 -- This is basically Pull's entire functionality
-adjustConsumeLinksInSymLink :: EvalMap -> FilePath -> FilePath -> IO [Link]
+adjustConsumeLinksInSymLink :: EvalMap -> FilePath -> FilePath -> FilePath -> IO [Link]
 adjustConsumeLinksInSymLink eval newRoot oldRoot oldTarget = do
-  mDotLinks <- getDotLinkIn oldRoot
+  mDotLinks <- LinkIO.getDotLinkIn oldRoot
   case mDotLinks of
     Just dotLinks -> adjustConsumeLinks eval newRoot oldRoot oldTarget dotLinks
     Nothing -> return []
@@ -44,8 +44,8 @@ adjustLink newRoot oldRoot oldTarget link@(source :=>: dest)
   | isAbsolute source = link  -- nothing to change, working with an explicit link
   | equalFilePath dest destFromSource = sourceFromRoot :=>: dest  -- not trying to put target inside source
   | otherwise = sourceFromRoot :=>: destViaTarget  -- special case
-  where destFromSource = makeRelative (root </> source) dest
+  where destFromSource = makeRelative (oldRoot </> source) dest
         sourceFromRoot = normalise $ deltaRoot </> source
-        destViaTarget = normalise . (oldTarget </>) . makeRelative root $ dest
-        deltaRoot = makeRelative root $ takeDirectory dotLinks  -- path to old root from new root
+        destViaTarget = normalise . (oldTarget </>) . makeRelative oldRoot $ dest
+        deltaRoot = makeRelative newRoot $ oldRoot  -- path to old root from new root
 
